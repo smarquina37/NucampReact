a893b64287d7930f266bcb566efde1359eb1b8ab
Susana Marquina <smarq86@gmail.com>
2021-01-24 13:58:46 -0800

    React Workshop 5

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -32,21 +32,25 @@ function Home(props) {
       <div className="container">
         <div className="row">
           <div className="col-md m-1">
-            <RenderCard 
-            item={props.campsite} 
-            isLoading={props.campsitesLoading}
-            errMess={props.campsitesErrMess}
-          />
+            <RenderCard
+              item={props.campsite}
+              isLoading={props.campsitesLoading}
+              errMess={props.campsitesErrMess}
+            />
           </div>
           <div className="col-md m-1">
-            <RenderCard 
-            item={props.promotion}
-            isLoading={props.promotionLoading}
-            errMess={props.promotionErrMess}
-             />
+            <RenderCard
+              item={props.promotion}
+              isLoading={props.promotionLoading}
+              errMess={props.promotionErrMess}
+            />
           </div>
           <div className="col-md m-1">
-            <RenderCard item={props.partner} />
+            <RenderCard
+              item={props.partner}
+              isLoading={props.partnersLoading}
+              errMess={props.partnersErrMess}
+            />
           </div>
         </div>
       </div>
